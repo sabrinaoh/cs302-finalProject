ee51ceef0a31fec63d24c4bd2a89ef79285c52c5
Sabrina Oh <sabrinaoh@nevada.unr.edu>
2020-04-29 12:15:39 -0700

    graph interface, only has pure virtual functions

---
+++ GraphInterface.h
@@ -0,0 +1,16 @@
+#ifndef GRAPH_INTERFACE_H_
+#define GRAPH_INTERFACE_H_
+
+template <typename T>
+class GraphInterface{
+    public:
+    virtual int getNumVertices() const = 0;
+    virtual int getNumEdges() const = 0;
+    virtual bool add (T start, T end, int edgeWeight) = 0;
+    virtual bool remove (T start, T end) = 0;
+    virtual int getEdgeWeight(T start, T end) = const = 0;
+    virtual void depthFirstTraversal (T start, void visit(T&)) = 0;
+    virtual void breadthFirstTraversal (T start, void visit(T&)) = 0;
+    virtual ~GraphInterface() {}
+};
+#endif
